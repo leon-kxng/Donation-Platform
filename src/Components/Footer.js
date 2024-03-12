3b78c481e09e2962d349eeabcc62559cc187acd9
Morgan-Ngetich <ngetichmorgan6@gmail.com>
2024-03-12 06:36:46 +0300

    Navbar and footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,47 +1,100 @@
 import React from 'react';
+import { IoIosMail } from "react-icons/io";
+import { FaPhone } from "react-icons/fa6";
+import { FaMapMarkerAlt } from "react-icons/fa";
 import './Footer.css';
 
-const Footer = () => {
-    return (
-        <div>
-            <div className="footer-separator"></div>
-            <footer className="footer-container">
-                <div className="footer-nav">
-                    <div className="footer-links-container">
-                        <a href="#">Home</a>
-                        <a href="#">Donation</a>
-                        <a href="#">About</a>
-                        <a href="#">Blog</a>
+function Footer() {
+  return (
+    <div className="footer">
+        <div className="main-footer">
+            <div className="footer-header">
+                <div className="logo-container">
+                    <img src="https://shorturl.at/yILN9" alt="Onation.jpg"/>
+                </div>
+
+                <div className="footer-navlinks">
+                    <ul>
+                        <a className="foot-link" href="#">Home</a>
+                        <a className="foot-link" href="#">About</a>
+                        <a className="foot-link" href="#">Donation</a>
+                        <a className="foot-link" href="#">Blog</a>
+                        <a className="foot-link" href="#">Pages</a>
+                        <a className="foot-link" href="#">Contact</a>
+                    </ul>
+                </div>
+
+                <div className="footer-donate">
+                    <button>Donate</button>
+                </div>
+            </div>
+
+
+            <div className="footer-body">
+                <div className="column-1">
+                <h5 className="column-head">Explore Links</h5> 
+                <div className="foot-links">
+                        <a className="foot-link" href="#">About Company</a>
+                        <a className="foot-link" href="#">Latest Projects</a>
+                        <a className="foot-link" href="#">Latest Blog</a>
+                        <a className="foot-link" href="#">Our stories</a>
+                        <a className="foot-link" href="#">Our Mission</a>               
                     </div>
-                    <div className="donate-button-container">
-                        <button className="donate-button">Donate</button>
+                </div>
+
+                <div className="column-1">
+                <h5 className="column-head">Get Support</h5> 
+                <div className="foot-links">
+                        <a className="foot-link" href="#">About</a>
+                        <a className="foot-link" href="#">How it works</a>
+                        <a className="foot-link" href="#">Knowledge Hub</a>
+                        <a className="foot-link" href="#">Success Stories</a>
+                        <a className="foot-link" href="#">Contact</a>                 
                     </div>
                 </div>
-                <div className="footer-section">
-                    <h2>Explore Links</h2>
-                    <p>About Company</p>
-                    <p>Latest Projects</p>
-                    <p>Latest Blog</p>
-                    <p>Our Testimonials</p>
-                    <p>Our Mission</p>
+
+                <div className="column-1">
+                <h5 className="column-head">Contact Details</h5> 
+                <div className="foot-links">
+                        <a className="foot-link" href="#"><IoIosMail /> onation@gmail.com</a>
+                        <a className="foot-link" href="#"><FaPhone /> (+88) 111-222-333</a>
+                        <a className="foot-link" href="#"><FaMapMarkerAlt/> Tropical Cyclone, Volcano</a>                 
+                    </div>
                 </div>
-                <div className="footer-section">
-                    <h2>Get Support</h2>
-                    <p>About</p>
-                    <p>How it Works</p>
-                    <p>Knowledge Hub</p>
-                    <p>Success Stories</p>
-                    <p>Contact</p>
+
+                <div className="column-1">
+                <h5 className="column-head">Projects</h5>
+                <div className="projects-container">
+                        <div className="projects-item">
+                            <div className="projects-image">
+                                <img src="https://shorturl.at/ioUV3" alt="projects-image" />
+                            </div>
+                            <div className="projects-details">
+                                <p style={{color: 'orange'}}>12.Oct.2024</p>
+                                <p >18 Best Charity Marketing Champions</p>
+                            </div>
+                        </div>
+
+                        <div className="projects-item">
+                            <div className="projects-image">
+                                <img src="https://shorturl.at/ioUV3" alt="projects-image" />
+                            </div>
+                            <div className="projects-details">
+                                <p style={{color: 'orange'}}>20.Oct.2024</p>
+                                <p>Charity starts from home. You Can't Even Help</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="footer-section">
-                    <h2>Contact</h2>
-                    <p>donation@gmail.com</p>
-                    <p>(+88) 111-222-333</p>
-                    <p>Tropical Cyclone, Volcano</p>
-                </div>
-            </footer>
+            </div>
+
+            <div className="footer-row">
+                <p className="copyright-text">Copyright © 2024 initTheme. All rights reserved.</p>
+            </div>
         </div>
-    );
+
+    </div>
+  )
 }
 
 export default Footer;
